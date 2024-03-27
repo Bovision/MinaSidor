15ec2704391e9c929a4e9b035b5f5b411e3a05fd
BoVision-Martin <martin@bovision.se>
2024-03-27 14:12:14 +0100

    UserAdminModal small dropdown fix

--- minasidor.client/src/Components/Modal/ModalComponents/ModalUserCard.tsx
+++ minasidor.client/src/Components/Modal/ModalComponents/ModalUserCard.tsx
@@ -1,18 +1,61 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 // import "./ObjectCard.css";
 import IUser from '../../../Interfaces/IUser';
+import { UserContext } from '../../../Context/UserContext';
+
+
+
 
 function ModalUserCard({ user } : { user: IUser}) {
+  
+  const { users } = useContext(UserContext);
+
+  let dropdownText;
+    switch(user.rights[0]) { 
+        case "officeAdmin": { 
+            //statements; 
+            dropdownText = "Kontorsadministratör"
+            break; 
+        } 
+        case "user": { 
+            //statements; 
+            dropdownText = "Användare"
+            break; 
+        } 
+        default: { 
+            //statements; 
+            dropdownText = "Kunde inte läsa"
+            break; 
+        } 
+    } 
+  
+let loggedInAdmin = false;
+if (users![0].rights[0] == "officeAdmin") {
+  loggedInAdmin = true;
+}
+
+
+
   return (
     <div className='ModalUser'>
       <div className='ModalUserCardLeft'>
         <img src={user.img} />
         <p>{user.firstName} {user.lastName}</p>
       </div>
-
       <div className='ModalUserCardRight'>
-        <p>Rättigheter-dropdown</p>
+        <select >
+            <option value="" disabled selected>{dropdownText}</option>
+            {/* Nedan skall bara vara synlig om det är en SuperAdmin som är inloggad */}
+            {/* <option value={"Val1"}>SuperAdmin</option> */}
+
+            {loggedInAdmin ? 
+              <option value={"Val2"}>Kontorsadmin</option>
+              : 
+              ""
+            }
+            <option value={"Val3"}>Användare</option>
+        </select>
       </div>
 
     </div>
